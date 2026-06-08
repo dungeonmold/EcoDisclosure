15fa925c9ec83e478bc9b59bcc39ed56b043de55
dungeonmold <mild_potatoes@proton.me>
2026-06-08 17:01:09 -0400

    Add files via upload

--- client/app.js
+++ client/app.js
@@ -2,7 +2,7 @@
 // CONFIGURATION
 // ======================================================
 
-const API_URL = "api/report";
+const API_URL = "/api/report";
 let map, propertyMarker, layerGroups = {}, legendControl, layerControl;
 
 const LAYER_STYLES = {
